640ad2e9e661a81c51ab6aaa3a9dff36f0ab9d74
abir <abir.bhattacharjee2580@gmail.com>
2025-02-27 16:40:34 +0530

    added new polyfills

--- js/9-feb-pollyfils/myFilter.js
+++ js/9-feb-pollyfils/myFilter.js
@@ -1,24 +1,29 @@
-const arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5];
 
+// Define custom myFilter function
+// input  - callback function (userFn)
+// each element iteration (passes element, index, and array)
+// returns - new array with elements that satisfy the condition
+if (!Array.prototype.myFilter) {
+  Array.prototype.myFilter = function (userFn) {
+    const result = [];
 
-// myFilter
-if(!Array.prototype.myFilter) {
-    Array.prototype.myFilter = function(userFn) {
-        const result = []
-        for(let i=0; i< this.length; i++) {
-            if (userFn(this[i])) result.push(this[i])
+    for (let i = 0; i < this.length; i++) {
+      if (i in this) {
+        if (userFn(this[i], i, this)) {
+          result.push(this[i]);
         }
-        return result
+      }
     }
+    return result;
+  };
 }
 
-
-
-const newArr = arr.filter((num) => num % 2 == 0)
+const newArr = arr.filter((num) => num % 2 == 0);
 console.log(newArr);
 
-const newArr2 = arr.myFilter((num) => num % 2 == 0)
+const newArr2 = arr.myFilter((num) => num % 2 == 0);
 console.log(newArr2);
 
-const arr3 = arr.myFilter((num)=> num > 2)
+const arr3 = arr.myFilter((num) => num > 2);
 console.log(arr3);
--- js/9-feb-pollyfils/myReduce.js
+++ js/9-feb-pollyfils/myReduce.js
@@ -1,16 +1,30 @@
-const arr = [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 4, 5];
 
-if(!Array.prototype.myReduce) {
-    Array.prototype.myReduce = function (userFn) {
-        let res = 0
-        for(let i=0; i<=this.length; i++){
-            res = userFn[i, i+1]
-        }
-        return res
+// Define myReduce function
+// input  - callback function (userFn), optional initialValue
+// process - applies reducer function on each element, accumulating result
+// output  - single accumulated value
+
+if (!Array.prototype.myReduce) {
+  Array.prototype.myReduce = function (userFn, initialValue) {
+    let accumulator, startIndex;
+
+    if (initialValue !== undefined) {
+      accumulator = initialValue;
+      startIndex = 0; // Start iteration from index 0
+    } else {
+      accumulator = this[0];
+      startIndex = 1; 
     }
+
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = userFn(accumulator, this[i], i, this);
+    }
+    return accumulator; 
+  };
 }
 
-let val = arr.reduce((accumulator, currentVal) => accumulator + currentVal, 0 )
- val = arr.myReduce((accumulator, currentVal) => accumulator + currentVal, 0 )
+let val = arr.reduce((accumulator, currentVal) => accumulator + currentVal, 0);
+val = arr.myReduce((accumulator, currentVal) => accumulator + currentVal, 0);
 
 console.log(val);
