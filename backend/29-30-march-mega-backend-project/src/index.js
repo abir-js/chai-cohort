c7212176d485e7c1f8e985fc114e7112d0679a34
abir <abir.bhattacharjee2580@gmail.com>
2025-04-05 02:27:07 +0530

    added 29 March code

---
+++ backend/29-30-march-mega-backend-project/src/index.js
@@ -0,0 +1,20 @@
+import app from "./app";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongodb connection error", error);
+    process.exit(1);
+  });
