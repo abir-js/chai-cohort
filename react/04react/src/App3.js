b0b39d08c20a152a29d6a44bf87c2e442e2ccc4a
abir <abir.bhattacharjee2580@gmail.com>
2025-05-19 13:02:01 +0530

    React day 1 - 17 May

---
+++ react/04react/src/App3.js
@@ -0,0 +1,23 @@
+import React from 'https://esm.sh/react@19.1.0';
+import ReactDOM from 'https://esm.sh/react-dom@19.1.0/client';
+
+const Chai = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('p', {}, props.cost),
+  ]);
+};
+
+const App3 = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Chai variations by ChaiCode'),
+    React.createElement(Chai, { name: 'Masala Chai', cost: 1200 }),
+    React.createElement(Chai, { name: 'Ginger Chai', cost: 1200 }),
+    React.createElement(Chai, { name: 'Lemon Tea', cost: 1200 }),
+    React.createElement(Chai, { name: 'Ice Tea', cost: 1200 }),
+  ]);
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App3));
