7d36176f8f78d64194c5bcdbf8faa68ad62ae33d
abir <abir.bhattacharjee2580@gmail.com>
2025-05-30 20:23:55 +0530

    24 May - tanstact router

---
+++ react/08react/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return <h2>Chai aur Router</h2>
+}
+
+export default App
