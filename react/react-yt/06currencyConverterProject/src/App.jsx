ab46c2eecbecd3e2aa1e4c82e0336a471fea477a
abir <abir.bhattacharjee2580@gmail.com>
2025-06-07 20:20:16 +0530

    changed directory name

--- react/react-yt/06currencyConverterProject/src/App.jsx
+++ react/react-yt/06currencyConverterProject/src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 import "./App.css";
 
 function App() {
-  
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
   return (
     <>
       <h1 className="text-3xl bg-orange-500">Currency app by Abir</h1>
--- react/react-yt/06currencyConverterProject/src/hooks/useCurrencyInfo.js
+++ react/react-yt/06currencyConverterProject/src/hooks/useCurrencyInfo.js
@@ -8,10 +8,10 @@ function useCurrencyInfo(currency) {
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((res) => setData(res[currency]))
+      .then((res) => setData(res[currency]));
   }, [currency]);
 
-  return data
+  return data;
 }
 
-export default useCurrencyInfo
+export default useCurrencyInfo;
