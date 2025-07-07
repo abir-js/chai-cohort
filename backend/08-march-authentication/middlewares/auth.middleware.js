1456417237185f64c7d542e9cc8ef6f12e186454
abir <abir.bhattacharjee2580@gmail.com>
2025-07-07 11:32:55 +0530

    add isLoggedIn middleware for token verification

---
+++ backend/08-march-authentication/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+export const isLoggedIn = async (req, res, next) => {
+  try {
+    //? 1. get token from cookies
+    console.log(req.cookies);
+    const token = req.cookies?.token;
+    console.log("Token found: ", token ? "Yes" : "No");
+
+    if (!token) {
+      console.log("no token");
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or unauthorized token token",
+      });
+    }
+
+    //? 2. verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log("Auth middleware failure");
+    return res.status(400).json({
+      success: false,
+      message: "Internal sercer error",
+    });
+
+  }
+
+  next();
+};
