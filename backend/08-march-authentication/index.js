3e280fafccc0e2512181327211d68429f0b13aba
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-08-29 09:12:23 +0530

    initialized project

--- backend/08-march-authentication/index.js
+++ backend/08-march-authentication/index.js
@@ -1,41 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import db from "./utils/db.js";
-import cookieParser from "cookie-parser";
-
-
-// import all routes
-import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-//?cors
-app.use(
-  cors({
-    origin: process.env.BASE_URL,
-    credentials: true,
-    methods: ["GET", "POST", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
 app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+  res.send("Hello World!");
 });
 
-//? Connecting to database
-await db();
-
-//? User Routes
-app.use("/api/v1/users", userRoutes);
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log("Example app listening on port 3000!");
 });
