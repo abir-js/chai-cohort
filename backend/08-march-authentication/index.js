adad335f7403124ea9914b36df5e196e7b1e0ac9
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-09-01 23:50:20 +0530

    added required middlewares

--- backend/08-march-authentication/index.js
+++ backend/08-march-authentication/index.js
@@ -1,10 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
