533b3a6edea9a78292f491d19858ce14aa8ba07f
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2025-10-18 20:18:25 +0530

    refactor: convert healthCheck to async function and add user model import

--- backend/29-30-march-mega-backend-project/src/controllers/healthcheck.controller.js
+++ backend/29-30-march-mega-backend-project/src/controllers/healthcheck.controller.js
@@ -1,9 +1,12 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { User } from "../models/user.model.js";
 
-const healthCheck = (req, res) => {
-  return res
-    .status(200)
-    .json(new ApiResponse(200, { message: "Server is up and running" }));
+const healthCheck = async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, { message: "Server is up and running" }));
+  } catch (error) {}
 };
 
-export {healthCheck}
+export { healthCheck };
